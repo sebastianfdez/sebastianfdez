2b8fb21da6628373093bf1f3be3da9a6067339d9
Sebastian <sfernandez@clever-age.com>
2021-03-23 00:14:27 +0100

    sebastianfdez to the world

---
+++ src/app/coming-soon/coming-soon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sf-coming-soon',
+  templateUrl: './coming-soon.component.html'
+})
+
+export class SFComingSoonComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
